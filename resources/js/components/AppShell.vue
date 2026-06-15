265257bd3b9650eb4c14c7ff41587f98563ade40
KakaniEmpires10 <muhammadalimkakani@gmail.com>
2026-06-15 12:42:22 +0700

    fix/ bug sidebar collpase

--- resources/js/components/AppShell.vue
+++ resources/js/components/AppShell.vue
@@ -14,16 +14,16 @@ const props = withDefaults(defineProps<Props>(), {
     forceSidebarCollapsed: false,
 });
 
-const isOpen = usePage().props.sidebarOpen;
-const sidebarOpen = ref(Boolean(isOpen));
+const page = usePage();
+const sidebarOpen = ref(Boolean(page.props.sidebarOpen));
+
+function sinkronkanSidebarOpen(): void {
+    sidebarOpen.value = props.forceSidebarCollapsed ? false : Boolean(page.props.sidebarOpen);
+}
 
 watch(
-    () => props.forceSidebarCollapsed,
-    (forceCollapsed) => {
-        if (forceCollapsed) {
-            sidebarOpen.value = false;
-        }
-    },
+    [() => props.forceSidebarCollapsed, () => page.props.sidebarOpen],
+    () => sinkronkanSidebarOpen(),
     { immediate: true },
 );
 </script>
@@ -32,7 +32,7 @@ watch(
     <div v-if="variant === 'header'" class="flex min-h-screen w-full flex-col">
         <slot />
     </div>
-    <SidebarProvider v-else v-model:open="sidebarOpen" :default-open="isOpen">
+    <SidebarProvider v-else v-model:open="sidebarOpen" :default-open="Boolean(page.props.sidebarOpen)">
         <slot />
     </SidebarProvider>
 </template>
